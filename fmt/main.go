97cc0ab633d740faea8e723b34f1fe44fe7ec36a
Marcos Augusto <msaorc@hotmail.com>
2022-11-16 11:51:27 -0300

    Learning about FMT

--- fmt/main.go
+++ fmt/main.go
@@ -7,11 +7,14 @@ type NewType int
 var (
 	a NewType = 2
 	b string  = "Hello World"
+	d bool    = false
 )
 
 func main() {
 	c := "Assigning new value"
-	fmt.Printf("The type of the variable (C) is: %T \n\n", c)
-	c = fmt.Sprintf("The type of the variable (A) is: %T", a)
+	c = fmt.Sprintf("The type of the variable (C) is: %T and value %v", c, c)
+	fmt.Printf("The type of the variable (A) is: %T and value %v \n\n", a, a)
+	fmt.Printf("The type of the variable (B) is: %T and value %v \n\n", b, b)
 	fmt.Println(c)
+	fmt.Printf("\nThe type of the variable (D) is: %T and value %v", d, d)
 }
